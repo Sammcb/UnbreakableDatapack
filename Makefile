1b8fbd6fddb4c87f1a1ca2c0340294d308d7defa
Samuel McBroom <sam@sammcb.com>
2026-03-03 15:13:15 -0800

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,18 @@
+.PHONY: build build-datapack clean lint
+.DEFAULT_GOAL := build
+
+DATAPACK_ZIP_NAME := unbreakable.zip
+
+build: build-datapack
+
+build-datapack: clean
+	(cd datapack && zip -q -r "../$(DATAPACK_ZIP_NAME)" data pack.mcmeta pack.png)
+
+clean:
+	rm -f $(DATAPACK_ZIP_NAME)
+
+lint:
+	zizmor .
+	editorconfig-checker
+	flake-checker --no-telemetry
+	nix flake check
---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+	description = "Unbreakable datapack";
+
+	inputs = {
+		# Commit does not correspond to a tag.
+		# Updating to latest commit generally follows unstable branch.
+		nixpkgs.url = "github:NixOS/nixpkgs/ce8de01ca4e1fdd238963bc91b104a21b2638743";
+		# Commit does not correspond to a tag.
+		flake-parts.url = "github:hercules-ci/flake-parts/f20dc5d9b8027381c474144ecabc9034d6a839a3";
+		flake-checker = {
+			# Commit corresponds to tag v0.2.11.
+			url = "github:DeterminateSystems/flake-checker/1d9e9f37224ff2904b7dbca5cad576725ee814e5";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = inputs@{self, nixpkgs, flake-parts, flake-checker}: flake-parts.lib.mkFlake {inherit inputs;} {
+		systems = ["aarch64-darwin" "aarch64-linux" "x86_64-linux"];
+		perSystem = {pkgs, system, ...}:
+			let
+				flakeCheckerPackage = flake-checker.packages.${system}.default;
+			in {
+				devShells.default = pkgs.mkShellNoCC {
+					nativeBuildInputs = [flakeCheckerPackage] ++ (with pkgs; [coreutils gnumake zip editorconfig-checker zizmor cosign trufflehog]);
+				};
+
+				devShells.build = pkgs.mkShellNoCC {
+					nativeBuildInputs = with pkgs; [gnumake zip];
+				};
+
+				devShells.lintWorkflows = pkgs.mkShellNoCC {
+					nativeBuildInputs = with pkgs; [zizmor];
+				};
+
+				devShells.lintEditorconfig = pkgs.mkShellNoCC {
+					nativeBuildInputs = with pkgs; [editorconfig-checker];
+				};
+
+				devShells.releaseArtifacts = pkgs.mkShellNoCC {
+					nativeBuildInputs = with pkgs; [gh];
+				};
+
+				devShells.createChecksums = pkgs.mkShellNoCC {
+					nativeBuildInputs = with pkgs; [coreutils];
+				};
+
+				devShells.sign = pkgs.mkShellNoCC {
+					nativeBuildInputs = with pkgs; [cosign];
+				};
+
+				devShells.scanSecrets = pkgs.mkShellNoCC {
+					nativeBuildInputs = with pkgs; [trufflehog];
+				};
+			};
+	};
+}
